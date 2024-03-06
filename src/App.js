105a63f7a453c772173be37a094a0138a6fae19a
MoeFingers <mbzuiter@gmail.com>
2024-03-06 00:55:10 -0700

    finished project whew imagine making commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,  { useEffect, useState } from 'react';
+import MoleHill from './MoleHill.jsx';
 
 function App() {
+
+  let [score, setScore] = useState(0)
+  let [timeLeft, setTimeLeft] = useState()
+  let [countDown, setCountDown] = useState()
+  let [status, setStatus] = useState("Press Start to begin!")
+  let [message, setMessage] = useState(["message-good", "placeholder good message"])
+  let [gameState, setGameState] = useState("pregame")
+  
+  const gameMessages = [
+    {time: 10, message: ["message-good", "Go whacky on them!"]},
+    {time: 9, message: ["message-bad", "They're coming out of the guacamole!"]},
+    {time: 6, message: ["message-bad", ""]},
+    {time: 5, message: ["message-mid", "Halfway through!"]},
+    {time: 3, message: ["message-bad", "Running out of time!"]},
+  ]
+
+  const missMessages = [
+    "Guac but no mole!",
+    "You suck!", 
+    "There's no mole there!"
+  ]
+
+  const hitMessages = [
+    "You are whack!",
+    "Caught him dipping!", 
+    "So whacky!",
+  ]
+  
+  //start game
+  // && trigger countdown with useeffect
+  // && set game state countdown
+  const startGame = () => {
+    setScore(0)
+    setCountDown(3)
+    setGameState("countdown")
+  }
+
+ //countdown useeffect which ultimately triggers timeleft use effect
+ //&& set game state active
+  useEffect(() => {
+    if (countDown > 0) {
+      setStatus("Game starts in: " + countDown)
+      intervalToZero(countDown, setCountDown)
+    } else if (countDown <= 0) {
+      setStatus("")
+      setGameState("active")
+      setTimeLeft(10)
+    }
+  }, [countDown])
+
+  //timeleft use effect
+  //&& set message
+  //&& set game state
+  useEffect(() => {
+    if(timeLeft > 0) {
+      intervalToZero(timeLeft, setTimeLeft)
+    } else if (timeLeft <= 0) {
+      endGame()
+    }
+    gameMessages.forEach(message => {
+      if (timeLeft === message.time) {
+        setMessage(message.message)
+      }
+    })
+  }, [timeLeft])
+
+const endGame = () => {
+    setGameState("over")
+    setStatus("Game Over! Final Score: " + score)
+  }
+
+  const intervalToZero = (time, setTime) => {
+    let interval = setInterval(() => {
+      if (time > 0) {
+        time--
+        setTime(time)
+      } else {
+        clearInterval(interval)
+      }
+    }, 1000)
+  }
+
+
+  
+
+  const createHills = () => {
+    let hills = []
+    for (let count = 0; count < 9; count++) {
+      hills.push(<MoleHill
+          key={count}
+          setScore={setScore}
+          score={score} 
+          message={message}
+          setMessage={setMessage}
+          missMessages={missMessages}
+          hitMessages={hitMessages}
+          />)
+    }
+    return <div className='mole-hill-container'>{hills}</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>G<strong>whac</strong>-A-Mole</h1>
+      <h2>{status}</h2>
+
+      {gameState === "pregame" ?
+        <button onClick={startGame} className='start-button'>Start</button>
+        : null }
+
+      {gameState === "active" ?
+        <div className='game-container'>
+        <div className='score-time-bar'>
+          <h2>Score: {score}</h2>
+          <h2>Time left: {timeLeft}</h2>
+        </div>
+        {createHills()}
+        <h3 className={message[0]}>{message[1]}</h3>
+      </div> : null } 
+
+
+      
+      {gameState === "over"
+        ? 
+        
+        <button onClick={startGame} className='start-button'>Restart</button>
+        :
+        null
+      }
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/MoleHill.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import hillpng from './hill.png'
+import hillwmolepng from './hillwmole.png'
+import { useEffect } from "react"
+
+function MoleHill(props) {
+    let [moleActive, setMoleActive] = useState(false)
+
+    function randomMessage(messages) {
+        return messages[Math.floor(Math.random() * messages.length)]
+    }
+    
+    function hillClicked() {
+        if (!moleActive) {
+            props.setScore(props.score -1)
+            props.setMessage(["message-bad", randomMessage(props.missMessages)])
+
+        } else if (moleActive) {
+            setMoleActive(false)
+            props.setScore(props.score +1)
+            props.setMessage(["message-good", randomMessage(props.hitMessages)])
+        } else {
+            console.log("so is it active or not!? - MoleHillContainer.jsx")
+        }
+    }
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 1000)
+        let otherRandSeconds = 200 +  Math.ceil(Math.random() * 1000)
+        let timer = setTimeout(() => {
+            setMoleActive(true)
+            setTimeout(() => {
+                setMoleActive(false)
+            }, otherRandSeconds);
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+
+    return (
+        <div className="mole-hill">
+            <img onClick={hillClicked} src={moleActive ? hillwmolepng : hillpng} alt={moleActive ? "A mole sprouting from a hill." : "A hill with no mole."} />
+        </div>
+    )
+}
+
+export default MoleHill
